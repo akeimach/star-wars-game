893453401f71bf328e3d611f3ded61e35a28ea53
AKmach <akeimach@users.noreply.github.com>
2017-10-16 11:54:57 -0700

    detect card location in id

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,7 +29,8 @@ $(document).ready(function(){
     var chooseHero;
     var chooseEnemy;
     var enemiesRemaining;
-
+    var gameOver;
+    var detachedCards = [];
 
     function initializeGame() {
         hero = null;
@@ -39,6 +40,7 @@ $(document).ready(function(){
         chooseHero = true;
         chooseEnemy = true;
         enemiesRemaining = Object.keys(allCharacters).length - 1;
+        gameOver = false;
         $('#fight-commentary-1, #fight-commentary-2').empty();
         $('#instruction').html('Choose your character');
     }
@@ -86,66 +88,45 @@ $(document).ready(function(){
 
     $('#attack').on("click", function() {
 
+        if (gameOver) return;
+
         $('#fight-commentary-1, #fight-commentary-2').empty();
 
-        var inBounds = [];
-
         for (var key in allCharacters) {
-            var position = $(key).position();
-            if (position.top < $('#arena').position().top + $('#arena').outerHeight()) {
-                inBounds.push(key);
-            }
-        }
-
-        if (inBounds.length !== 2) {
-            $('#fight-commentary-1').html('Place exactly two players in the arena.');
-            $('#fight-commentary-2').html('Rearrange your set up.');
-            return;
-        }
-        else {
-            if (chooseHero && chooseEnemy) {
-                if (($(inBounds[0]).position().left < $(inBounds[1]).position().left)) {
-                    heroKey = inBounds[0];
-                    enemyKey = inBounds[1];
-                }
-                else {
-                    heroKey = inBounds[1];
-                    enemyKey = inBounds[0];
-                }
-                chooseHero = false;
-                chooseEnemy = false;
-                hero = allCharacters[heroKey];
-                enemy = allCharacters[enemyKey];
-            }
-            else if (chooseEnemy) {
-                if (($(inBounds[0]).position().left < $(inBounds[1]).position().left)) {
-                    enemyKey = inBounds[1];
-                }
-                else {
-                    enemyKey = inBounds[0];
-                }
-                if ($(enemyKey).position().left < $(heroKey).position().left) {
-                    $('#fight-commentary-1').html('You must play the same character every round.');
-                    $('#fight-commentary-2').html('Rearrange your set up.');
-                    return;
-                }
-                else {
-                    chooseEnemy = false;
-                    enemy = allCharacters[enemyKey];
-                    enemiesRemaining--;
-                    if (enemiesRemaining > 0) {
-                        $('#instruction').html('Up next:');
+            if (detachedCards.indexOf(key) === -1) { // if the character hasn't been detached
+                var position = $(key).position();
+                if ((Math.abs(position.top - $('#hero-box').position().top) < 100) &&
+                    (Math.abs(position.left - $('#hero-box').position().left) < 100)) {
+                    if (chooseHero) {
+                        chooseHero = false;
+                        heroKey = key;
+                        hero = allCharacters[heroKey];
                     }
                     else {
-                        $('#instruction').empty();
+                        $('#fight-commentary-1').html('You cant change your hero.');
+                        $('#fight-commentary-2').html('Rearrange your set up.');
                     }
                 }
-
-            } 
+                if ((Math.abs(position.top - $('#enemy-box').position().top) < 100) &&
+                    (Math.abs(position.left - $('#enemy-box').position().left) < 100)) {
+                    if (chooseEnemy) {
+                        chooseEnemy = false;
+                        enemyKey = key;
+                        enemy = allCharacters[enemyKey];
+                        enemiesRemaining--;
+                        if (enemiesRemaining > 0) {
+                            $('#instruction').html('Up next:');
+                        }
+                        else {
+                            $('#instruction').empty();
+                        }
+                    }
+                }
+            }
         }
         
         if (chooseHero) {
-            $('#fight-commentary-1').html('You need to confirm your cards.');
+            $('#fight-commentary-1').html('You need to choose your cards.');
             return;
         }
         else if (chooseEnemy) {
@@ -156,12 +137,14 @@ $(document).ready(function(){
         enemy.healthPoints -= hero.attackPower; // Hero attacks first
         if (enemy.healthPoints < 1) {
             $(enemyKey).detach();
+            detachedCards.push(enemyKey);
             $('#fight-commentary-1').html('You have defeated ' + enemy.displayName + '.');
             if (enemiesRemaining > 0) {
                 chooseEnemy = true;
                 $('#fight-commentary-2').html('Choose your next enemy.');
             }
             else {
+                gameOver = true;
                 $('#fight-commentary-2').html('You won the game!');
                 $('#instruction').empty();
             }
@@ -171,6 +154,8 @@ $(document).ready(function(){
             hero.healthPoints -= enemy.counterAttackPower;
             if (hero.healthPoints < 1) {
                 $(heroKey).detach();
+                detachedCards.push(heroKey);
+                gameOver = true;
                 $('#fight-commentary-1').html('You lost to ' + enemy.displayName + '.');
                 $('#fight-commentary-2').html('Game over.');
             }
